3aff4bd3268a0644c1fd1385b325fcd2bd495c3a
Scott k. Fraley <scott.k.fraley@gmail.com>
2018-08-03 15:12:06 -0700

    Updated several of the UI js libs and got the spinner working. DONE!

---
+++ PRHawkSkf/Scripts/Home_Index.js
@@ -0,0 +1,27 @@
+﻿
+// displays a wait spinner
+function ShowWaitSpinner() {
+
+	$.blockUI({
+		css: {
+			border: "none",
+			backgroundColor: "transparent",
+			opacity: .9,
+			color: "#000"
+		},
+		overlayCSS: { backgroundColor: "#686868" },
+		message: $("#spinner")
+	});
+
+	setTimeout($.unblockUI, 120000);
+};
+
+$(document).ready(function() {
+
+	// handles the temp spinner button click event to display the spinner
+	// sets a 120 second timeout
+	$("#goButton").on("click", function() {
+		ShowWaitSpinner();
+	});
+
+});
